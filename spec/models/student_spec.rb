39d4c177f19b101784f571cf71f4999996c57974
Adam Sickels <47608860+adamsickels00@users.noreply.github.com>
2020-11-19 21:42:57 -0500

    added model tests

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'is valid without attributes' do
+    expect(Student.new).to be_valid
+  end
+  
 end
